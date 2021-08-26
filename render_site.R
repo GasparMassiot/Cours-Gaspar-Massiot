25b63f7e71d9f42987796541711dd5c70d6ad384
GasparMassiot <gaspar.massiot@gmail.com>
2021-08-26 17:33:55 +0200

    Trender update

--- render_site.R
+++ render_site.R
@@ -1,7 +1,10 @@
+# On créé le dossier du site au cas où (peut être pas nécessaire)
 system('mkdir -p build') 
 
+# render_site, va juste render index.Rmd normalement
 rmarkdown::render_site(encoding = 'UTF-8')
 
+# Pour render les fichiers Rmd des sous-dossiers
 dirs <- dir('.', pattern = "^_[^(site)]")
 filenames <- NULL
 for (d_ in dirs){
@@ -14,8 +17,10 @@ for(f_ in filenames){
   rmarkdown::render(f_)
 }
 
+# On envoie dans le dossier du site, pour moi il se nomme build
 dirs <- dir('.', pattern = "^_[^(site)]", full.names = T)
 
 for (d_ in dirs){
   system( glue::glue( 'cp -rf {d_} build/. '))
+  print(d_)
 }
